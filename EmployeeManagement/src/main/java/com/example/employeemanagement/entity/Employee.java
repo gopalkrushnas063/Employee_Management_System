3d2189bef8e0519e142b46f8ba77165eb8e9a5b1
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-04-02 12:20:33 +0530

    Employee Management System

---
+++ EmployeeManagement/src/main/java/com/example/employeemanagement/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.example.employeemanagement.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+
+
+}
